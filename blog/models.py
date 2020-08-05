f53ae3732c531fc30ddfe8a0f940dc1c6705b361
eunji <exk986@student.bham.ac.uk>
2020-08-05 19:26:35 +0900

    Blog extension

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -14,6 +13,23 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+        
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
